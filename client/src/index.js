0b95697b357fbb1ff26747dd9ffc3246392ee072
oreoluwade <oreoluwade@gmail.com>
2017-12-01 07:40:19 +0100

    configure react app for hot reloading as well as automatic browser opening upon bundle.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <AppContainer>
+    <App />,
+  </AppContainer>,
+  document.getElementById('root'),
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,9 +8,13 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 
 export default {
-  entry: './src/index.js',
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     filename: 'bundle.js',
   },
   module: {
@@ -26,8 +30,17 @@ export default {
       },
     ],
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    noInfo: true,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
 };
