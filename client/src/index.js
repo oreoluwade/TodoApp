679594e81bffc4932769db0c75dcb5efac31f415
oreoluwade <oreoluwade@gmail.com>
2017-12-01 13:49:30 +0100

    Wrap the app with the BrowserRouter component to enable routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
 ReactDOM.render(
-  <AppContainer>
-    <App />,
-  </AppContainer>,
+  <Router>
+    <AppContainer>
+      <App />
+    </AppContainer>
+  </Router>,
   document.getElementById('root'),
 );
