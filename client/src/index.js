e5d13b42f73e1f04a0ac353debbfd6571b26c038
oreoluwade <oreoluwade@gmail.com>
2017-12-02 13:51:14 +0100

    Wrap the whole App with the MuiThemeProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
+import { MuiThemeProvider } from 'material-ui/styles';
 import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
+
 ReactDOM.render(
-  <Router>
-    <AppContainer>
-      <App />
-    </AppContainer>
-  </Router>,
+  <MuiThemeProvider>
+    <Router>
+      <AppContainer>
+        <App />
+      </AppContainer>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root'),
 );
