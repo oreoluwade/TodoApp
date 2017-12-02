6177b9900841c6bd546d2259a927d20079ba8a4f
oreoluwade <oreoluwade@gmail.com>
2017-12-02 16:09:50 +0100

    refactor homepage component

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 const HomePage = () => (
-  <p>testing router</p>
+  <Card>
+    <CardHeader
+      title="Hompage"
+      subtitle="Subtitle"
+      actAsExpander={true}
+      showExpandableButton={true}
+    />
+    <CardActions>
+      <FlatButton label="Action1" />
+      <FlatButton label="Action2" />
+    </CardActions>
+    <CardText>
+      Welcome! Here, you can create Todos and
+      mark them as completed when you are done.
+    </CardText>
+  </Card>
 );
 
 export default HomePage;
