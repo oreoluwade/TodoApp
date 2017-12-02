d7a9522d4352e8521e094aaeaabbf28bd523efbd
oreoluwade <oreoluwade@gmail.com>
2017-12-02 13:50:24 +0100

    add Navbar as a permanent component in App

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,10 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 import { store } from '../store';
 import { NotFound } from './NotFound';
 import { HomePage } from './HomePage';
+import { NavBar } from './common';
 
 const App = () => (
   <Provider store={store}>
     <Switch>
+      <NavBar/>
       <Route path="/" exact component={HomePage}></Route>
       <Route path="*" component={NotFound}></Route>
     </Switch>
