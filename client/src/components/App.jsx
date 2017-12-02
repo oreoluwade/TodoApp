8e55689bb671f23fa6c5b984a61209c70c576b3b
oreoluwade <oreoluwade@gmail.com>
2017-12-02 16:06:19 +0100

    refactor App component and entry point

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
+import { MuiThemeProvider } from 'material-ui/styles';
+import styled from 'styled-components';
 import { store } from '../store';
 import { NotFound } from './NotFound';
 import { HomePage } from './HomePage';
 import { NavBar } from './common';
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  height: 100%;
+`;
+
 const App = () => (
   <Provider store={store}>
-    <Switch>
-      <NavBar/>
-      <Route path="/" exact component={HomePage}></Route>
-      <Route path="*" component={NotFound}></Route>
-    </Switch>
+    <MuiThemeProvider>
+      <Wrapper>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={HomePage}></Route>
+          <Route path="*" component={NotFound}></Route>
+        </Switch>
+      </Wrapper>
+    </MuiThemeProvider>
   </Provider>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-roboto';
-import { MuiThemeProvider } from 'material-ui/styles';
 import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 
-
 ReactDOM.render(
-  <MuiThemeProvider>
-    <Router>
-      <AppContainer>
-        <App />
-      </AppContainer>
-    </Router>
-  </MuiThemeProvider>,
+  <Router>
+    <AppContainer>
+      <App />
+    </AppContainer>
+  </Router>,
   document.getElementById('root'),
 );
